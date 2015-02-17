fad6a051369bbbc8e135f8938412f4f3fda6a7f7
Viet Truong <viettruong2194@gmail.com>
2015-02-17 00:22:38 -0800

    Style work page

--- assets/js/magister.js
+++ assets/js/magister.js
@@ -11,6 +11,25 @@ jQuery(document).ready(function($) {
 	$("a", '.mainmenu').click(switchSection);
 	$('.btn-contact').click(switchSection);
 
+	$('.btn.btn-no-filter').click(function() {
+		$('.work-thumbnail').removeClass('thumbnail-hide');
+	});
+
+	$('.btn.btn-website').click(function() {
+		$('.work-website').removeClass('thumbnail-hide');
+		$('.work-wordpress, .work-application').addClass('thumbnail-hide');
+	});
+
+	$('.btn.btn-wordpress').click(function() {
+		$('.work-wordpress').removeClass('thumbnail-hide');
+		$('.work-website, .work-application').addClass('thumbnail-hide');
+	});
+
+	$('.btn.btn-application').click(function() {
+		$('.work-application').removeClass('thumbnail-hide');
+		$('.work-website, .work-wordpress').addClass('thumbnail-hide');
+	});
+
 	function switchSection() {
 		if( ! $(this).hasClass('active') ) { 
 			current_item = this;
