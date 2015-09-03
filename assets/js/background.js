6c9623c584c1eee2fea3c6872826794161468b2a
Viet Truong <viettruong2194@gmail.com>
2015-09-03 00:22:26 -0700

    New update

---
+++ assets/js/background.js
@@ -0,0 +1,19 @@
+$('.btn-head').click(function() {
+    $('html').removeClass();
+    $('html').addClass('head');
+});
+
+$('.btn-about').click(function() {
+    $('html').removeClass();
+    $('html').addClass('about');
+});
+
+$('.btn-work').click(function() {
+    $('html').removeClass();
+    $('html').addClass('work');
+});
+
+$('.btn-contact').click(function() {
+    $('html').removeClass();
+    $('html').addClass('contact');
+});
